97040a8c61937ce63b97fdf78c3e60960429c276
ibrahimislam10 <ibrahimislam101010@gmail.com>
2022-05-05 04:29:08 +0600

    delete

--- index.js
+++ index.js
@@ -37,6 +37,7 @@ async function run() {
             res.send(result)
         });
         
+        //delete method
         app.delete('/services/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
